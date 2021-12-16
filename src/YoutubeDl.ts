37f1bc06c5c6d4f93171bdab9ffeddb83a73043d
Gilbert142 <gijsh55@gmail.com>
2021-12-16 02:10:48 +0100

    Shallow caching

--- src/YoutubeDl.ts
+++ src/YoutubeDl.ts
@@ -5,19 +5,49 @@ const path = require('path');
 const isWin = process.platform === "win32";
 const bin = path.resolve('tools/bin/youtube-dl' +(isWin ? '.exe' : ''));
 
+const cache = new Map();
+const intervalCache = new Map();
+
+function setCache(key: string, data: any): void {
+    cache.set(key, data);
+
+    if (!intervalCache.has(key)) intervalCache.set(key, setInterval(() => refreshCache(key), 60000));
+}
+
+function refreshCache(command: string) {
+    console.log(`Refreshing cache for ${command}...`);
+
+    exec(command, (error: ExecException | null, stdout: string, stderr: string) => {
+        if (error) return console.error(`Failed to exec refresh cache for ${command}: `, error.stack);
+        try {
+            let resultObject = JSON.parse(stdout);
+            cache.set(command, resultObject);
+            console.log(`Cached result for ${command}: ${stdout}`);
+        } catch (e) {
+            console.error(`Failed to refresh cache for ${command}: `, (e as Error).stack);
+        }
+    });
+}
+
 export class YoutubeDl {
     public static async getVideoMetadata(url: string, options?: string, schema?: string[]) {
         options = options ||  '-f \"best\"';
         const command = `${bin} ${options} --dump-single-json ${url}`;
+
+        if (cache.has(command)) return schema ? YoutubeDl.filterKeys(cache.get(command), schema) : cache.get(command);
+
         return await new Promise<any>((resolve, reject) => {
+            console.log(`Executing ${command}...`);
             exec(command, (error: ExecException | null, stdout: string, stderr: string) => {
-                if(error) {
+                if (error) {
                     reject({error: error.message, stderr, stdout});
                     return
                 }
                 try {
                     let resultObject = JSON.parse(stdout);
-                    if(schema) {
+                    setCache(command, resultObject);
+                    console.log(`Cached result for ${command}: ${stdout}`);
+                    if (schema) {
                         resultObject = YoutubeDl.filterKeys(resultObject, schema);
                     }
                     resolve(resultObject);
